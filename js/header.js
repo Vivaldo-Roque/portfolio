f631bccc274801f47c855cd239f2504b31464900
Vivaldo-Roque <2001vivaldo@gmail.com>
2022-11-24 00:01:22 +0100

    header caption

--- js/header.js
+++ js/header.js
@@ -1,21 +1,26 @@
+var clicked = false;
+var delayInMilliseconds = 2000;
+
 window.addEventListener("DOMContentLoaded", () => {
 
     var header = document.getElementById("home");
     var caption = document.getElementById("header_caption");
 
     header.addEventListener('click', function (event) {
+
+        caption.classList.toggle("caption_hide");
+
         var x = event.clientX - 70;
         var y = event.clientY - 40;
-        caption.style.display = "block";
         caption.style.left = `${x}px`;
         caption.style.top = `${y}px`;
-    })
+        caption.style.opacity = 0.8;
+        setTimeout(function () {
+            caption.style.opacity = 0;
+        }, delayInMilliseconds);
 
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+    });
 });
+
+
 
