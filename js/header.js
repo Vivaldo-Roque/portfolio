c71a6beb15e31c448845136d3f60e7bd39a93d03
Vivaldo-Roque <2001vivaldo@gmail.com>
2022-12-10 09:25:57 +0100

    fixed caption positions

--- js/header.js
+++ js/header.js
@@ -2,90 +2,114 @@ var clicked = false;
 var delayInMilliseconds = 2000;
 var centerX = 0;
 var centerY = 0;
+var myTimeout;
 
 window.addEventListener("DOMContentLoaded", () => {
 
     var header = document.getElementById("home");
     var caption = document.getElementById("header_caption");
 
-    header.addEventListener('mousemove', (event) => {
-        centerX = header.offsetWidth / 2;
-        centerY = header.offsetHeight / 2;
-    });
+    centerX = header.offsetWidth / 2;
+    centerY = header.offsetHeight / 2;
 
     header.addEventListener('click', (event) => {
 
-        var hBound = HorizontallyBound(header, caption);
-        var vBound = VerticallyBound(header, caption);
+        clearTimeout(myTimeout);
 
-        caption.classList.toggle("caption_hide");
-
-        var x = event.clientX - 70;
-        var y = event.clientY - 40;
+        var x = event.offsetX - 70;
+        var y = event.offsetY - 40;
 
         caption.style.left = `${x}px`;
         caption.style.top = `${y}px`;
 
+        var out = isOutside(header, caption);
+
         if((centerX < x) && (y < centerY)){
-            // Direita cima
-            // console.log("Direita cima")
-            if(hBound){
-                // caption.style.left = `0px`
+            //Direita cima
+            console.log("Direita cima")
+            if(out.right){
+                caption.style.right = `0px`
+                caption.style.left = `auto`
             }
 
-            if(vBound){
+            if(out.top){
                 caption.style.top = `0px`
+                caption.style.bottom = `auto`
             }
         } else if((centerX < x) && (y > centerY)){
             // Direita baixo
-            // console.log("Direita baixo")
-            if(hBound){
-                // caption.style.left = `0px`
+            console.log("Direita baixo")
+            if(out.right){
+                 caption.style.right = `0px`
+                 caption.style.left = `auto`
             }
 
-            if(vBound){
+            if(out.bottom){
                 caption.style.bottom = `0px`
+                caption.style.top = `auto`
             }
         } else if((x < centerX) && (y < centerY)){
             // Esquerda cima
-            // console.log("Esquerda cima")
-            if(hBound){
+            console.log("Esquerda cima")
+            if(out.left){
                 caption.style.left = `0px`
+                caption.style.right = `auto`
             }
 
-            if(vBound){
+            if(out.top){
                 caption.style.top = `0px`
+                caption.style.bottom = `auto`
             }
         } else if((x < centerX) && (y > centerY)){
             // Esquerda baixo
-            // console.log("Esquerda baixo")
-            if(hBound){
+            console.log("Esquerda baixo")
+            if(out.left){
                 caption.style.left = `0px`
+                caption.style.right = `auto`
             }
 
-            if(vBound){
+            if(out.bottom){
                 caption.style.bottom = `0px`
+                caption.style.top = `auto`
             }
         }    
 
         caption.style.opacity = 0.8;
-        setTimeout(function () {
+        myTimeOut = setTimeout(function () {
             caption.style.opacity = 0;
         }, delayInMilliseconds);
 
     });
 });
 
-function VerticallyBound(parentDiv, childDiv) {
-    var parentRect = parentDiv.getBoundingClientRect();
-    var childRect = childDiv.getBoundingClientRect();
+function isOutside(parent, child) {
+    var box1coords = parent.getBoundingClientRect();
+    var box2coords = child.getBoundingClientRect();
 
-    return parentRect.bottom <= childRect.bottom && parentRect.top >= childRect.top;
-}
+    var outside = {
+        top: false,
+        bottom: false,
+        left: false,
+        right: false
+    };
+  
+    if (box2coords.top < box1coords.top) {
 
-function HorizontallyBound(parentDiv, childDiv) {
-    var parentRect = parentDiv.getBoundingClientRect();
-    var childRect = childDiv.getBoundingClientRect();
+        outside.top = true;
+    }
 
-    return parentRect.left <= childRect.left && parentRect.right >= childRect.right;
-}
+    if(box2coords.right > box1coords.right){
+        outside.right = true;
+    }
+
+    if(box2coords.bottom > box1coords.bottom){
+        outside.bottom = true;
+    }
+
+    if(box2coords.left < box1coords.left){
+        outside.left = true;
+    }
+    
+    return outside;
+
+  }
