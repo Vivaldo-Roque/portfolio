a366ead506fbc842a4453a762f1210eea8d88506
Vivaldo-Roque <2001vivaldo@gmail.com>
2025-06-26 21:46:58 +0100

    fix mediaqueries

--- js/collapsibles.js
+++ js/collapsibles.js
@@ -1,7 +1,6 @@
 window.addEventListener("load", () => {
     document.querySelectorAll('.collapsible-header').forEach(header => {
     const group = header.nextElementSibling;
-    const arrow = header.querySelector('.arrow');
     header.addEventListener('click', () => {
       header.classList.toggle('open');
       group.classList.toggle('open');
